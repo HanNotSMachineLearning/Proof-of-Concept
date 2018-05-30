2f5fa917b9b30a63716623a0fcc0f4db64d07887
Robin Bozan <robinbozan@gmail.com>
2018-05-30 11:30:05 +0200

    POST-method toegevoegd

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
-from flask import request, url_for, render_template
+from flask import request, url_for, render_template, abort
 from flask_api import FlaskAPI, status, exceptions
+import json
 
 app = FlaskAPI(__name__)
+
+available_symptoms = ('test', 'skrt')
 
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return render_template('index.html', available_symptoms=available_symptoms)
+
+
+@app.route('/foo', methods=['POST'])
+def foo():
+    if not request.form:
+        abort(400)
+
+    print(request.form.getlist('symptoms'))
+    return json.dumps(request.form)
 
 if __name__ == "__main__":
     app.run(debug=True)
