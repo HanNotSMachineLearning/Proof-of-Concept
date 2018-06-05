d377117b774f348b626f13c6030b9a16267c9287
Sander Teunissen <sander.teunissen@home.nl>
2018-06-05 13:09:23 +0200

    Fixed complete frontend

--- predictor.py
+++ predictor.py
@@ -35,28 +35,24 @@ clf = clf.fit(features, labels)
 def predict(gender, age, symptoms):
     gender = bool(gender)
     age = int(age)
-
+ 
     existing_symptoms = list(
         filter(lambda v: v in available_symptoms, symptoms))
-
+ 
     if len(existing_symptoms) != len(symptoms):
         not_existing_symptoms = list(
             set(symptoms) - set(existing_symptoms))
         raise Exception(
             "U mag alleen symptomen opnoemen die bij ons geregistreerd zijn. De symptomen die u invulde maar niet bij ons geregistreerd staan zijn: " + ",".join(not_existing_symptoms))
-
+ 
     symptoms_array = [gender, age]
     for available_symptom in available_symptoms:
         symptoms_array.append(1 if available_symptom in symptoms else 0)
-
+ 
     predictions = clf.predict_proba([symptoms_array])[0]
-    best_prediction_value = max(predictions)
-    print("Value = " + str(best_prediction_value))
-    best_prediction_index = predictions.index(best_prediction_value)
-
-    print(clf.predict_proba([symptoms_array]))
-
-    prediction = int(clf.predict([symptoms_array])[0])
-    print(ziektes[best_prediction_index])
-
-    return ziektes[prediction]
+    value, index = max([(v, i) for i, v in enumerate(predictions)])
+ 
+    return {
+        'disease': ziektes[index],
+        'chance': value
+    }
--- server.py
+++ server.py
@@ -9,25 +9,31 @@ import predictor
 
 app = FlaskAPI(__name__)
 
-
 @app.route('/')
 def root():
-    return render_template('index.html', available_symptoms=predictor.available_symptoms)
+    return render_template('index.html')
 
 
-@app.route('/api/available_symptoms')
+@app.route('/api/symptoms')
 def available_symptoms():
     return predictor.available_symptoms
 
+@app.route('/api/diseases')
+def available_duseases():
+    return predictor.ziektes
 
-@app.route('/foo', methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def foo():
     if not request.form:
         abort(400)
-
     result = predictor.predict(
         request.form['gender'], request.form['age'], request.form.getlist('symptoms'))
-    return result
+    gender = 'man'
+    if request.form['gender'] == 0:
+        gender = 'vrouw'
+    return render_template('result.html',disease=result['disease'],accuracy=round(float(result['chance'])*100, 2),gender=gender,age=request.form['age'],symptoms=request.form.getlist('symptoms'))
+
 
 if __name__ == "__main__":
     context = ('ssl/machine.crt', 'ssl/machine.key')
